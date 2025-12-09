d87db1611fdda79a70d8559f62a936cc52a154a8
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-12-08 21:21:00 -0600

    vite initilize

--- Frontend/src/Main.jsx
+++ Frontend/src/Main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from 'App.jsx'
+import './Public/Index.css'
+import Landing from './Components/Landing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <>
+      <Landing />
+    </>
   </StrictMode>,
 )
