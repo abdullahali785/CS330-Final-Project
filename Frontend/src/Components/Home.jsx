364409012aaa905afd3945518b84fc22327307fc
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-12-10 22:23:37 -0600

    Landing + Info + Home Header

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -2,7 +2,17 @@
 function Home() {
     return (
         <div>
-            
+            <div class="container"> 
+                <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
+                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"><span class="fs-4">RideShare</span> </a> 
+                    <ul class="nav nav-pills"> 
+                        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li> 
+                        <li class="nav-item"><a href="#" class="nav-link">Add a ride</a></li> 
+                        <li class="nav-item"><a href="#" class="nav-link">Trips</a></li> 
+                        <li class="nav-item"><a href="#" class="nav-link">Sign Out</a></li> 
+                    </ul> 
+                </header> 
+            </div>
         </div>
     )
 }
--- Frontend/src/Main.jsx
+++ Frontend/src/Main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Public/Index.css'
 import Landing from './Components/Landing.jsx'
+import Info from './Components/Info.jsx'
+import Home from './Components/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <Landing />
+      <Info />
+      <Home />
     </>
   </StrictMode>,
 )
