a7541ef92e999a859149e5c9380fdf9988cd1190
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-12-16 01:35:48 -0600

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@Assets': path.resolve(__dirname, './src/Assets'),
-    },
-  },
+  base: '/',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
